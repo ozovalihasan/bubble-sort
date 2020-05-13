fca83cb2a9159bbd063af7549507fcffd01263c4
Hasan Özovalı <ozovalihasan@gmail.com>
2020-05-13 16:18:23 +0300

    Add bubble_sort_by method and fix errrors found by rubocop

--- main.rb
+++ main.rb
@@ -1,26 +1,31 @@
-
-
-def bubble_sort( arr )
-  (arr.size - 1).times do | counter |
-    p counter 
-    p arr[ counter ] , arr[ counter + 1 ] 
-    p '============'
-    if arr[ counter ] > arr[ counter + 1 ]
-      arr[ counter ] , arr[ counter + 1 ] =  arr[ counter +1 ] , arr[ counter ]
-    end    
-
+def bubble_sort(arr)
+  loop do
+    changed = false
+    (arr.size - 1).times do |counter|
+      if arr[counter] > arr[counter + 1]
+        arr[counter], arr[counter + 1] = arr[counter + 1], arr[counter]
+        changed = true
+      end
+    end
+    break unless changed
   end
-  p arr
 end
 
-def bubble_sort_by()
-  yield [ 2, 3 ] 
+def bubble_sort_by(arr1)
+  loop do
+    changed = false
+    (arr1.length - 1).times do |counter|
+      if (yield [arr1[counter], arr1[counter + 1]]).positive?
+        arr1[counter], arr1[counter + 1] = arr1[counter + 1], arr1[counter]
+        changed = true
+      end
+    end
+    break unless changed
+  end
 end
 
-bubble_sort_by() do |left,right|
-  left - right
+bubble_sort_by(%w[hallo salut hi hello hey merhaba]) do |left, right|
+  left.length - right.length
 end
 
-bubble_sort([3,2,1,3,5,67])
-
-
+bubble_sort([2, 1, 1, 3, 5, 67])
