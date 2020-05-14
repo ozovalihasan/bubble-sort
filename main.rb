453c5ac4c2464f4bd3509c19194199897588aec8
Hasan Özovalı <ozovalihasan@gmail.com>
2020-05-14 11:29:16 +0300

    Fix errors found by rubocop

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 def bubble_sort(arr)
   counter2 = 0
-  loop do 
+  loop do
     changed = false
     (arr.size - 1 - counter2).times do |counter|
       if arr[counter] > arr[counter + 1]
@@ -9,23 +9,25 @@ def bubble_sort(arr)
       end
     end
     break unless changed
+
     counter2 += 1
-    break if counter2 > arr.size - 2     
+    break if counter2 > arr.size - 2
   end
   arr
 end
 
 def bubble_sort_by(arr1)
   counter2 = 0
-  loop do 
+  loop do
     changed = false
-    (arr1.length - 1 - counter2 ).times do |counter|
+    (arr1.length - 1 - counter2).times do |counter|
       if (yield [arr1[counter], arr1[counter + 1]]).positive?
         arr1[counter], arr1[counter + 1] = arr1[counter + 1], arr1[counter]
         changed = true
       end
     end
     break unless changed
+
     counter2 += 1
     break if counter2 > arr1.size - 2
   end
@@ -38,4 +40,4 @@ end
 
 p result
 
-p bubble_sort([ 67, 30,3 ,2,1])
+p bubble_sort([67, 30, 3, 2, 1])
